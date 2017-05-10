8e5e8e730dacec1328e1e7c0cc96f26263d2bb52
sidmadethis <sidmadethis@gmail.com>
2017-05-10 01:16:05 -0600

    making instance from a class

--- dog.py
+++ dog.py
@@ -24,3 +24,15 @@ class Dog():
 # __init__ is a special method python runs automatically. self is special and must come first.  every method call associated with a class must reference to the instance itself, which is what self does?
 # it gives the individual instance access to the attributes and methods in the class.
 # so if we made an instance of dog, python will call the __init__ method, we don't need to pass self (it is done automatically) and we would pass name and age
+
+# making an instance of this class
+
+my_dog = Dog('Willie', 6)
+# the init statement above has no explicit return statement, but python automatically returns an instance representing this dog
+
+print("My dog's name is " + my_dog.name.title()+".")
+print("My dog is "+ str(my_dog.age)+" years old.")
+
+
+my_dog.sit()
+my_dog.roll_over()
